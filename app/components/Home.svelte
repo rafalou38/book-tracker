de3adf5ecf11b2c74022e59b846eea2e504783a0
rafalou38 <rafael.martinezcalvo@orange.fr>
2022-11-02 19:06:35 +0100

    tutorial

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,130 @@
-<page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
-</page>
-
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+  import {
+    action,
+    ItemEventData,
+    TapGestureEventData,
+  } from "@nativescript/core";
+  import { Template } from "svelte-native/components";
+
+  async function actionWrapper(
+    message: string,
+    cancelButtonText: string,
+    actions: string[]
+  ): Promise<string> {
+    await new Promise((resolve) => setTimeout(resolve, 150));
+    return action(message, cancelButtonText, actions);
+  }
+
+  let todos: { name: string }[] = [];
+  let dones: { name: string }[] = []; //completed items go here
+  const removeFromList = <T>(list: T[], item: T) =>
+    list.filter((t) => t !== item);
+  const addToList = <T>(list: T[], item: T) => [item, ...list];
+  let textFieldValue = "";
+
+  async function onItemTap(args: ItemEventData) {
+    let result = await actionWrapper(
+      "What do you want to do with this task?",
+      "Cancel",
+      ["Mark completed", "Delete forever"]
+    );
+
+    console.log(result); // Logs the selected option for debugging.
+    let item = todos[args.index];
+    switch (result) {
+      case "Mark completed":
+        dones = addToList(dones, item); // Places the tapped active task at the top of the completed tasks.
+        todos = removeFromList(todos, item); // Removes the tapped active task.
+        break;
+      case "Delete forever":
+        todos = removeFromList(todos, item); // Removes the tapped active task.
+        break;
+      case "Cancel" || undefined: // Dismisses the dialog
+        break;
+    }
+  }
+  async function onDoneTap(args: ItemEventData) {
+    let result = await actionWrapper(
+      "What do you want to do with this  task?",
+      "Cancel",
+      ["Mark To Do", "Delete forever"]
+    );
+
+    console.log(result); // Logs the selected option for debugging.
+    let item = dones[args.index];
+    switch (result) {
+      case "Mark To Do":
+        todos = addToList(todos, item); // Places the tapped active task at the top of the completed tasks.
+        dones = removeFromList(dones, item); // Removes the tapped active task.
+        break;
+      case "Delete forever":
+        dones = removeFromList(dones, item); // Removes the tapped active task.
+        break;
+      case "Cancel" || undefined: // Dismisses the dialog
+        break;
+    }
+  }
+
+  function onButtonTap() {
+    if (textFieldValue === "") return; // Prevents users from entering an empty string.
+    console.log("New task added: " + textFieldValue + "."); // Logs the newly added task in the console for debugging.
+    todos = [{ name: textFieldValue }, ...todos]; // Adds tasks in the ToDo array. Newly added tasks are immediately shown on the screen.
+    textFieldValue = ""; // Clears the text field so that users can start adding new tasks immediately.
+  }
 </script>
 
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
+<page>
+  <actionBar title="My Tasks" />
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+  <tabView>
+    <tabViewItem title="To Do">
+      <gridLayout columns="*,120" rows="70,*">
+        <!-- Configures the text field and ensures that pressing Return on the keyboard
+				produces the same result as tapping the button. -->
+        <textField
+          col={0}
+          row={0}
+          bind:text={textFieldValue}
+          hint="Type new task..."
+          editable="true"
+          on:returnPress={onButtonTap}
+        />
+        <button
+          col={1}
+          row={0}
+          text="Add task"
+          on:tap={onButtonTap}
+          class="-primary"
+        />
+
+        <listView items={todos} on:itemTap={onItemTap} row="1" colSpan="2">
+          <Template let:item>
+            <label text={item.name} textWrap="true" />
+          </Template>
+        </listView>
+      </gridLayout>
+    </tabViewItem>
+    <tabViewItem title="Completed">
+      <listView items={dones} on:itemTap={onDoneTap}>
+        <Template let:item>
+          <label text={item.name} textWrap="true" class="todo-item-completed" />
+        </Template>
+      </listView>
+    </tabViewItem>
+  </tabView>
+</page>
+
+<style>
+  textField {
+    font-size: 20;
+  }
+  label {
+    font-size: 18;
+  }
+
+  .todo-item-completed {
+    /* color: #939393; */
+    opacity: 0.7;
+    text-decoration: line-through;
+  }
 </style>
